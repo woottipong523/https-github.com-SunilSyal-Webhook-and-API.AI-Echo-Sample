e4ffc796a56a85a02b07816f86020edabf5bb03a
woottipong523 <woottipong523@psru.ac.th>
2018-10-04 11:33:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ restService.post("/echo", function(req, res) {
       : "Seems like some problem. Speak again.";
   return res.json({
     speech: speech,
-    displayText: speech,
+    displayText: "Woottipong Kongsib",
     source: "webhook-echo-sample"
   });
 });
